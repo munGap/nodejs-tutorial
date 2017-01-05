418fdcde80337482e51ca91825812ba39ffc1c10
munGap <wogmxal@naver.com>
2017-01-05 15:24:22 +0900

    delete unnecessary files

--- server/node_modules/azure-mobile-apps/src/data/sqlite/statements/columns.createTable.js
+++
@@ -1,9 +0,0 @@
-﻿// ----------------------------------------------------------------------------
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// ----------------------------------------------------------------------------
-
-module.exports = function () {
-    return {
-        sql: "CREATE TABLE [__types] ([table] TEXT, [name] TEXT, [type] TEXT)"
-    };
-};
